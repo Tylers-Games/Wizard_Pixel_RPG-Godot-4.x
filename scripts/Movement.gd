774f18dbcf053f21e93775008884080634c34e5d
Tyler Tanos <164990047+Tylertanos@users.noreply.github.com>
2026-02-10 01:16:59 +0800

    Create Movement.gd

---
+++ scripts/Movement.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+@export var speed := 120
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+
+	velocity = direction.normalized() * speed
+	move_and_slide()
